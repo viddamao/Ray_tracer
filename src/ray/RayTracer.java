c7fc13412b5f89d768cbef96370ced0349b03c8d
wei-zheng-chen <wei.zheng.chen@duke.edu>
2015-01-24 21:52:13 -0500

    added lambert code

--- src/ray/RayTracer.java
+++ src/ray/RayTracer.java
@@ -176,7 +176,9 @@ public class RayTracer {
         		}
         	}
         }
+        
         if (closest == null) {
+//        	System.out.println("HIT NULL");
             return BACKGROUND_COLOR;
         } else {
 //        	System.out.println(ray.evaluate(tmin));
